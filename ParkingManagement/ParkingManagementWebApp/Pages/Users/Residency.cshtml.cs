8eccdf8a3f8c40b45e2781db30c0e882496dccbd
NVinh02 <nguyncvinh129@gmail.com>
2022-07-14 11:17:24 +0700

    Fix bug

---
+++ ParkingManagement/ParkingManagementWebApp/Pages/Users/Residency.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BusinessObject.Models;
+using BusinessObject.BusinessModels;
+using ParkingManagementWebApp.Ultilities;
+using ParkingManagementWebApp.Constants;
+
+namespace ParkingManagementWebApp.Pages.Users
+{
+    public class ResidencyModel : PageModel
+    {
+        private readonly BusinessObject.Models.ParkingManagementContext _context;
+
+        public ResidencyModel(BusinessObject.Models.ParkingManagementContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            UserInformation userInformation = SessionHelper.GetFromSession<UserInformation>(HttpContext.Session, SessionValue.Authentication);
+            if (userInformation == null)
+            {
+                return RedirectToPage("/Accounts/Login");
+            }
+            if (!userInformation.IsStaff)
+            {
+                return RedirectToPage("/Index");
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.Users.FirstOrDefaultAsync(m => m.Ccid == id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            UserInformation userInformation = SessionHelper.GetFromSession<UserInformation>(HttpContext.Session, SessionValue.Authentication);
+            if (userInformation == null)
+            {
+                return RedirectToPage("/Accounts/Login");
+            }
+            if (!userInformation.IsStaff)
+            {
+                return RedirectToPage("/Index");
+            }
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.Users.FindAsync(id);
+
+            if (User != null)
+            {
+                User.IsResident = !User.IsResident;
+                _context.Users.Update(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
